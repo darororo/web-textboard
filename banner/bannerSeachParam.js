f9603082ac6bbeee926d07393bdb98441a3a42a7
Yem Daro <100412991+darororo@users.noreply.github.com>
2024-05-17 00:49:16 +0700

    query url parameters

---
+++ banner/bannerSeachParam.js
@@ -0,0 +1,8 @@
+// get url parameter values
+let url = window.location.href;
+let searchParams = new URL(url).searchParams;
+let entries = new URLSearchParams(searchParams);
+
+document.getElementById("text-input").value = searchParams.get("text");
+document.getElementById("color-input").innerHTML = searchParams.get("color");
+document.getElementById("font-input").innerHTML = searchParams.get("font");
